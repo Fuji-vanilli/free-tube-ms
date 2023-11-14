6eaf90832d0a97968b9846870bc14fb8498d9d30
Fuji vanilli <ijufillivan@gmail.com>
2023-11-14 16:43:16 +0300

    commit front

--- backend/video-service/src/main/java/com/freetube/videoservice/VideoServiceApplication.java
+++ backend/video-service/src/main/java/com/freetube/videoservice/VideoServiceApplication.java
@@ -2,12 +2,14 @@ package com.freetube.videoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.multipart.MultipartFile;
 
 @SpringBootApplication
 public class VideoServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VideoServiceApplication.class, args);
+
 	}
 
 }
